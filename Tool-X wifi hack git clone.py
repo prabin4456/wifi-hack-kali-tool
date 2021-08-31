f22c0020f2a46adcb734c0234197a6268fe2e369
Prabin <76979813+prabin4456@users.noreply.github.com>
2021-08-31 16:54:59 +0545

    Add files via upload

---
+++ Tool-X wifi hack git clone.py
@@ -0,0 +1,46 @@
+_____           _    __  __
+        |_   _|__   ___ | |   \ \/ /
+          | |/ _ \ / _ \| |____\  /
+          | | (_) | (_) | |____/  \
+          |_|\___/ \___/|_|   /_/\_\ v2.1
+
+
+ =============================================
+|          Install Best Hacking Tool          |
+ =============================================
+
+ [ 1 ] install Pyrit
+ [ 2 ] install RTLSDR-Scanner
+ [ 3 ] install WiFi-Pumpkin
+ [ 4 ] install WifiBruteCrack
+ [ 5 ] install aircrack-ng
+ [ 6 ] install airgeddon
+ [ 7 ] install cowpatty
+ [ 8 ] install crackle
+ [ 9 ] install fern-wifi-cracker
+ [ 10 ] install fluxion
+ [ 11 ] install giskismet
+ [ 12 ] install kalibrate-rtl
+ [ 13 ] install kickthemout
+ [ 14 ] install killerbee
+ [ 15 ] install mfcuk
+ [ 16 ] install mfoc
+ [ 17 ] install mfterm
+ [ 18 ] install multimon-ng
+ [ 19 ] install netattack2
+ [ 20 ] install netattack
+ [ 21 ] install pixiewps
+ [ 22 ] install reaver
+ [ 23 ] install wifi-hacker
+ [ 24 ] install WifiGod
+ [ 25 ] install wifiphisher
+ [ 26 ] install wifitap
+ [ 27 ] install wifite2
+ [ 28 ] install wifite
+ [ 29 ] install wifresti
+ [ 30 ] install wirespy
+
+ =============================================
+|  00) Back                                   |
+ =============================================
+ ##>
